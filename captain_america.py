bbc91c07d9f8efcf7e77c86bb751bf2cea3dd33a
Sachin Gurung <saching061@hotmail.com>
2022-05-17 22:25:48 +0000

    Captain America. Practice with Dictionary

---
+++ captain_america.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+captain_america = {"Real Name": "Steven Rogers", 
+            "First Appearance": "Captain America Comics #1 (March, 1941)",
+            "Creators":["Joe Simon", "Jack Kirby"],
+            "Team Affiliations": "Avengers",
+            "Alias": "Winghead"
+            }
+print(captain_america["Real Name"])
