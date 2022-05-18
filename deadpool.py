16a96b97f0f6fdbdb43c992237bea4ac0d03c4aa
Sachin Gurung <saching061@hotmail.com>
2022-05-18 19:57:08 +0000

    learning about dictionaries

---
+++ deadpool.py
@@ -0,0 +1,7 @@
+Deadpool={'Real Name':'Wade Winston Wilson',
+        'First Appearance':'New Mutants (Vol. 1) #98 (February, 1991)',
+        'Creators':['Fabian Nicieza','Rob Liefeld'],
+        'Team Affiliations':['Thunderbolts','X-Force','Agency X','Deadpool Corps.','Great Lakes Initiative','Weapon X','Landau','Luckman','X-Men'],
+        'Skills and abilities':['Master martial artist','swordsman and marksman','Master strategist and improviser','has several voices in his head the represent parts of himself that give him advice','easily defies expectation in most instances']}
+
+print(f"Good morning, I'm deadpool. My real name is {Deadpool['Real Name']}. My first appearance in was in {Deadpool['First Appearance']}. One of my creators are {Deadpool['Creators'][0]} and I'm affiliated with the team {Deadpool['Team Affiliations'][0]} and many more.")
