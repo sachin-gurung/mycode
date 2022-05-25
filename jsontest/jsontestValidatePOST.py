9ce98d352447ce809a50375f36a917538555f0d0
Sachin Gurung <saching061@hotmail.com>
2022-05-25 18:03:47 +0000

    GET vs POST

---
+++ jsontest/jsontestValidatePOST.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import requests
+from datetime import datetime
+
+# define the URL we want to use
+POSTURL = "http://validate.jsontest.com/"
+
+def main():
+    # test data to validate as legal json
+    # when a POST json= is replaced by the KEY "json"
+    # the key "json" is mapped to a VALUE of the json to test
+    # because the test item is a string, we can include whitespaces
+    mydata = {"json": "{'fruit': ['apple', 'pear'], 'vegetable': ['carrot']}" }
+
+    # use requests library to send an HTTP POST
+    resp = requests.post(POSTURL, data=mydata)
+
+    # strip off JSON response
+    # and convert to PYTHONIC LIST / DICT
+    respjson = resp.json()
+
+    # display our PYTHONIC data (LIST / DICT)
+    print(respjson)
+
+    # JUST display the value of "validate"
+    print(f"Is your JSON valid? {respjson['validate']}")
+
+    now = datetime.now()
+    print(now)
+
+if __name__ == "__main__":
+    main()
+
