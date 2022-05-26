11a2bb5bbbcb7615f64cce94c3114d3ee8703bc2
Sachin Gurung <saching061@hotmail.com>
2022-05-26 21:56:54 +0000

    Bare minimum working copy of final project. This meets the minimum requirement

---
+++ flaskapi/alta3research-flask01.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Final Flask Project"""
+
+from flask import Flask
+from flask import request
+from flask import redirect
+from flask import jsonify
+from flask import render_template
+
+app = Flask(__name__)
+data = [{
+    "student":"Sachin",
+    "alias": "Iron man",
+    "age" : 35,
+    "programming skills": [
+        "python",
+        "flask",
+        "api",
+        "java"
+    ]
+}]
+
+@app.route("/data")
+def userdata():
+    return jsonify(data)
+    
+
+@app.route("/user")
+def user():
+    return f"<h1>Welcome. You've reached the <u>USER</u> path.</h1>"
+    
+@app.route("/user/<name>")
+def anything(name):
+    return f"<h1>Welcome {name}<h1>\n"  
+
+@app.route("/<username>")
+def index(username):
+    return render_template("jinjapage.html", name = username)
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2224)
